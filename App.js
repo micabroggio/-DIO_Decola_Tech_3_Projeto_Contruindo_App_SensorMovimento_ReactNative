54fb1e60cb1b7f1a59d9e7209b3ed3236ec8e79d
micabroggio <micaoceano@gmail.com>
2022-06-30 18:23:41 -0300

    Projeto desenvolvimento de app com react native

---
+++ App.js
@@ -0,0 +1,71 @@
+import React,{useState,useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+
+const App = () => {
+
+  const [toggle,setToggle] = useState(false);
+
+  const pressChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    const subscription = RNShake.addListener(()=>{
+      setToggle(oldToggle => !oldToggle)
+    });
+    return () => subscription.remove();
+  }, []);
+
+  return <View style = {toggle ? style.containerLight : style.container} >
+    <TouchableOpacity onPress = {pressChangeToggle}>
+      <Image
+        style = {style.lightOnLogo}
+        source = {toggle 
+          ? require('C:/Users/micao/OneDrive/dev_git/projetos_decola_tech/01/flashApp/assets/icons/logo_branco.png') 
+          : require('./assets/icons/logo_preto.png')}
+      />
+      <Image
+        style = {style.lightOn}
+        source = {toggle 
+          ? require('C:/Users/micao/OneDrive/dev_git/projetos_decola_tech/01/flashApp/assets/icons/on.png') 
+          : require('./assets/icons/off.png')}
+      />
+    </TouchableOpacity>
+  </View>;
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    padding: 20,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight:{
+    flex:1,
+    padding: 20,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightOn:{
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    justifyContent:'center',
+  },
+  lightOnLogo:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 100,
+    height: 100,    
+  },
+})
